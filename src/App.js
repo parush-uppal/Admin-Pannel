20d86845ba48e1a135144872a6fefa6de708f067
parush-uppal <parush1008.be21@chitkara.edu.in>
2023-02-01 02:10:06 +0530

    Admin Pannel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Card from './Card'
+import Chart from './Chart'
+import Navbar from './Navbar'
+import Sidbar from './Sidbar'
+import Table from './Table'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <BrowserRouter>
+      <Navbar/>
+      <Sidbar/>
+      <Card/> 
+      <Routes>
+      <Route path="/" element={<Chart/>} />
+      <Route path="/table" element={<Table/>} />
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
